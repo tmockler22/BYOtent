8ea5686263d57af9b18608f4018cd429ccd578bc
Tristan <tmocklercoding@gmail.com>
2019-11-15 10:57:12 -0600

    Finish campsites controller

--- app/controllers/api/campsites_controller.rb
+++ app/controllers/api/campsites_controller.rb
@@ -1,2 +1,39 @@
 class Api::CampsitesController < ApplicationController
+
+  def create 
+    @campsite = Campsite.new(campsite_params)
+
+    if @campsite.save 
+      render 'api/campsites/show'
+    else 
+      render json: @campsite.errors.full_messages, status: 422
+    end
+  end
+
+  def index 
+    @campsites = Campsite.all 
+    render :index 
+  end
+
+  def show 
+    @campsite = Campsite.find(params[:id])
+    render :show
+  end
+
+  def campsite_params
+    params.require(:campsite).permit(
+           :title,
+           :description,
+           :type,
+           :owner_id,
+           :max_guests,
+           :daily_rate,
+           :address,
+           :state,
+           :zipcode,
+           :country, 
+           :lat, 
+           :lng, 
+        )
+  end
 end
